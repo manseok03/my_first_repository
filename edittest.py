407933cf1b3d561f58f74a854ae7f9d245975486
manseok03 <143787869+manseok03@users.noreply.github.com>
2023-09-04 18:29:06 +0900

    Add files via upload

---
+++ edittest.py
@@ -0,0 +1,16 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+print(b)
+print(b, a)
+print("python", a, b)
+
+print(number_school = 2394584903)
+print(str_name = "python")
+print(bool_view = False)
+print(flt_pi = 3.14)
+print(char_element = 'a')
+print(chars = "hello world")
+print("number : ", number_school, " name ", str_name, " bool : ", bool_view, "float : ", flt_pi, " charcter : " , char_element, " charcter : ", chars)
